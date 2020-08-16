e07311d524f84ea1aa0f7fa2fe660e6edb1d19fd
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-08-16 19:02:43 +0300

    123

---
+++ src/Core/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Core;
+
+class Config
+{
+    const MYSQL_HOST = 'localhost';
+    const MYSQL_USER_NAME = 'root';
+    const MYSQL_PASSWORD = 'root';
+    const MYSQL_DATABASE = 'guests_book';
+    const PAGE_SIZE = 10;
+//    const MYSQL_TABLE = 'phonebook';
+    // const MYSQL_INFO = 'phonebook';
+    const BLOCK_LIST = '../BlockList.json';
+    const SALT = '12345678910';
+}
