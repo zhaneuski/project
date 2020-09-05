b1c126adf9158b72afaba72557b3b3505a3c77fe
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-09-05 21:03:53 +0300

    133

--- src/Controller/LandlordNewsController.php
+++ src/Controller/LandlordNewsController.php
@@ -13,18 +13,18 @@ class LandlordNewsController extends AbstractTableController
 {
     protected $tableName = "news";
 
-    public function __construct(View $view)
-    {
-        parent::__construct($view);
-        $this->table = new NewsModel(
-            $this->tableName,
-            DB::Link([
-                'host' => Config::MYSQL_HOST,
-                'username' => Config::MYSQL_USER_NAME,
-                'password' => Config::MYSQL_PASSWORD,
-                'dbname' => Config::MYSQL_DATABASE
-            ])
-        );
-        $this->view->setFolder('landlordnews');
-    }
+//    public function __construct(View $view)
+//    {
+//        parent::__construct($view);
+//        $this->table = new NewsModel(
+//            $this->tableName,
+//            DB::Link([
+//                'host' => Config::MYSQL_HOST,
+//                'username' => Config::MYSQL_USER_NAME,
+//                'password' => Config::MYSQL_PASSWORD,
+//                'dbname' => Config::MYSQL_DATABASE
+//            ])
+//        );
+//        $this->view->setFolder('landlordnews');
+//    }
 }
